a89f528a6e8ed82366ad8e821ec7f092372849a4
Renemari Padillo <padillo.renemari@gmail.com>
2015-05-03 17:11:15 +0800

    Initial Commit

---
+++ angular/controllers/TweetController.js
@@ -0,0 +1,34 @@
+(function () {
+	
+	'use strict';
+	
+	angular.module('tweet-pa-more.controllers')
+           .controller('TweetController', TweetController);
+        
+    function TweetController($scope) {
+                
+        $scope.posts = [];
+        $scope.newPost = { created_by: '', message: '', created_at: '' };
+        
+        // Function Definitions
+        // ---------------------
+        
+        /**
+         * Submits the tweet.
+         */
+        $scope.PostTweet = function () {
+            
+            $scope.newPost.created_at = Date.now();
+            $scope.posts.push($scope.newPost);
+            
+            // Reset the scope data, so that we can re-use it again
+            $scope.newPost = {
+                created_by: '',
+                message: '',
+                created_at: ''
+            };
+        
+        };
+        
+    }
+})();
