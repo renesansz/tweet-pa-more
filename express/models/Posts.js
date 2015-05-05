f7e401f767200aa267182af91bc7d9ea45a1882c
Renemari Padillo <padillo.renemari@gmail.com>
2015-05-05 20:27:35 +0800

    Updated Post Schema Structure

--- express/models/Posts.js
+++ express/models/Posts.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-    user_id: {
-        type: String,
-        required: true
-    },
+    username: String,
     message: String,
     created_at: {
         type: Date,
